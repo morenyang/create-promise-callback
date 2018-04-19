4cc13dba1194a0271cf99fb37110284525d33c9f
Moren YANG <morenyang88@gmail.com>
2018-04-20 00:26:45 +0800

    fix bugs and add test methods

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ const createPromiseCallback = () => {
   let cb;
   if (!global.Promise) {
     global.Promise = require('bluebird');
-    console.warn(
-      `Your Node rumtime does not support Promise, now using bluebrid. `
-    );
-    console.warn(
-      `You can set "global.Promise" to your preferred Promise library. `
-    );
   }
 
   const promise = new global.Promise((resolve, reject) => {
     cb = function() {
       const [err, ...args] = arguments;
       if (err) return reject(err);
-      return resolve(args);
+      resolve(...args);
     };
   });
   cb.promise = promise;
---
+++ test/test.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const assert = require('chai').assert;
+const createPromiseCallback = require('../lib/index');
+
+function func(err, fn) {
+  fn = fn || createPromiseCallback();
+  setTimeout(() => {
+    if (err) return fn(err);
+    fn(null, 'done');
+  }, 0);
+  return fn.promise;
+}
+
+function multiArgsFunc(args, fn) {
+  fn = fn || createPromiseCallback();
+  setTimeout(() => {
+    fn(null, ...args);
+  }, 0);
+  return fn.promise;
+}
+
+describe('create-promise-callback module', () => {
+  it('should callback with "done"', done => {
+    func(null, (err, res) => {
+      assert.notOk(err);
+      assert.equal(res, 'done');
+      done();
+    });
+  });
+
+  it('should reslove with "done"', done => {
+    func(null).then(res => {
+      assert.equal(res, 'done');
+      done();
+    });
+  });
+
+  it('should callback with error', done => {
+    const error = new Error('error');
+    func(error, (err, res) => {
+      assert.equal(err, error);
+      assert.notOk(res);
+      done();
+    });
+  });
+
+  it('should reject promise with error', done => {
+    const error = new Error('error');
+    func(error)
+      .then(res => {
+        assert.notOk(res);
+      })
+      .catch(err => {
+        assert.equal(err, error);
+        done();
+      });
+  });
+
+  it('should reslove with multi arguments', done => {
+    const args = [1, 2, 3];
+    multiArgsFunc(args)
+    .then((arg1, arg2, arg3) =>{
+      assert.deepEqual([arg1, arg2, arg3][1,2,3])
+      done();
+    })
+  });
+
+  describe('using module without Promise', () =>{
+    before(() =>{
+      delete global.Promise;
+    })
+  
+    it('should use bluebird if global.Promise not defined', done =>{
+      func(null).then(res => {
+        assert.equal(res, 'done');
+        done();
+      });
+    })
+
+
+  })
+
+});
